a008f33b5995899df7df7138c9a1617b872ae89a
victor oyiga <victoroyiga01@gmail.com>
2026-03-30 15:08:24 +0100

    checking the frontend

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes,  Navigate } from "react-router-dom"
 
 const AppRoute =()=>{
     return(
        <Routes>
           <Route path="/" element={<h1 className="text-red-500 text-2xl">Home</h1>} />
           <Route path="/about" element={<h1 className="text-red-500 text-2xl">About</h1>} /> 
+          <Route path="*" element={<Navigate to="/" />} />
        </Routes>
     )
 }
